0061f16fb475c8823edafaab6e5928f459efb5b6
samdanTetteh <samdantettey@gmail.com>
2020-08-13 12:09:46 +0100

    implememted list data to show video list with paging error tracking

---
+++ app/src/main/java/com/ijikod/gmbn_youtube/ui/LoadingStateAdapter.kt
@@ -0,0 +1,29 @@
+package com.ijikod.gmbn_youtube.ui
+
+import android.view.ViewGroup
+import androidx.paging.LoadState
+import androidx.paging.LoadStateAdapter
+
+
+/**
+ * Loading adapter to work with [LoadingStateViewHolder]
+ * **/
+class LoadingStateAdapter(private val retry: () -> Unit) : LoadStateAdapter<LoadingStateViewHolder>() {
+
+    /** Populate view holder with [LoadState]
+     * **/
+    override fun onBindViewHolder(holder: LoadingStateViewHolder, loadState: LoadState) {
+        holder.bind(loadState)
+    }
+
+    /**
+     * Instantiate [LoadingStateViewHolder] with [retry] function to be invoked
+     * **/
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        loadState: LoadState
+    ): LoadingStateViewHolder {
+        return LoadingStateViewHolder.create(parent, retry = retry)
+    }
+
+}
