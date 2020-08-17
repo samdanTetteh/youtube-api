8d9d3bf67af602ee416f35cc0c176df68fbb7391
samdan <samdan@whizzogroup.com>
2020-08-17 08:58:52 +0100

    added publised date

--- app/src/main/java/com/ijikod/gmbn_youtube/ui/viewbinding/BindingAdapters.kt
+++ app/src/main/java/com/ijikod/gmbn_youtube/ui/viewbinding/BindingAdapters.kt
@@ -1,11 +1,13 @@
 package com.ijikod.gmbn_youtube.ui.viewbinding
 
+import android.os.Build
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.text.HtmlCompat
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.ijikod.gmbn_youtube.R
+
 
 
 /**
@@ -18,7 +20,19 @@ fun loadImage(imageView: ImageView, image : String?){
     }
 }
 
+/**
+ * A [BindingAdapter] to show HTML text
+ * **/
 @BindingAdapter("htmlTxt")
 fun loadDescription(textView: TextView, value : String?){
     textView.text = value?.let { HtmlCompat.fromHtml(it, HtmlCompat.FROM_HTML_MODE_LEGACY) }
+}
+
+
+/**
+ * A [BindingAdapter] to for formatted date
+ * **/
+@BindingAdapter("dateTxt")
+fun formatDateTime(textView: TextView, value: String?){
+    textView.text = value?.split("T")?.first()
 }
